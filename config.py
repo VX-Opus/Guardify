2407c512510c82ff1fe661bf45eaedb3e82efb9a
IKEX01 <ccchekingusa@gmail.com>
2025-03-14 20:15:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,21 +9,23 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 API_ID = 18136872
 API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+
+# Load environment variables
+load_dotenv()
+
+# Load values from environment variables
+OWNER_ID = int(getenv("OWNER_ID", default="6257927828"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", default="6257927828").split()))
 SUDO_USERS.append(OWNER_ID)
+SUDO_USERS.extend(DEV)  # Add DEV users if any
+
 SPOILER_MODE = os.environ.get("SPOILER_MODE", "True").lower() == "true"
 MONGO_URI = os.environ.get("MONGO_URI", "")
 DB_NAME = ""
-SUDO_ID = [6257927828]
 LOGGER = False
 BOT_NAME = "Guardify"
 SUPPORT_ID = -1002064111110
-
 BOT_TOKEN = getenv("BOT_TOKEN", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6257927828").split()))
-for x in DEV:
-    SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6257927828"))
-SUDO_USERS.append(OWNER_ID)
-
+# Initialize the bot client
 BOT = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
